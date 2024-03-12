f294826ff3f39f8733f5438a7800675b189749f5
lekha <lekha@LAPTOP-CQISO5OB>
2024-03-12 15:37:23 +0530

    first commit

---
+++ Corejava/src/com/tns/abstraction/StudentDemo.java
@@ -0,0 +1,15 @@
+package com.tns.abstraction;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student ob= new Student();
+		ob.setSerialnum(101);
+		ob.setName("manu");
+		ob.setAge(20);
+		System.out.println(ob);
+	}
+
+}
